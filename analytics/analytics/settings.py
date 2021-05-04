ecb8fcdccf98d66a20a043e48a64f103d42cfdf6
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2021-05-04 10:43:11 +0530

    Final Build

--- analytics/analytics/settings.py
+++ analytics/analytics/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,7 +74,9 @@ ROOT_URLCONF = 'analytics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -195,6 +197,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- analytics/analytics/urls.py
+++ analytics/analytics/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    path('', TemplateView.as_view(template_name='index.html'))
 
 ]
